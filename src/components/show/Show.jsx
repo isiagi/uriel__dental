54072deeea6438ff0355bbc73cb08630e790b486
isiagi <isiagigeofrey0@gmail.com>
2024-01-08 15:36:53 +0300

    tryfour

--- src/components/show/Show.jsx
+++ src/components/show/Show.jsx
@@ -1,12 +1,27 @@
 import "./show.css";
-import img1 from "../../assets1/images/19.jpg";
 import Button from "../button/Button";
+import ReactPlayer from "react-player";
 
 function Show() {
   return (
     <div className="show__container">
       <div className="middle__class show__wrapper">
-        <div className="show__image__div"></div>
+        <div className="show__image__div">
+          <ReactPlayer
+            width={"100%"}
+            height={"100%"}
+            url={
+              "https://res.cloudinary.com/isiagi/video/upload/v1704643602/loid5v7tpvdeci02hj5i.mp4"
+            }
+            pip={true}
+            controls={true}
+            light={false}
+            playing={true}
+            onError={() => {
+              console.log("Something went wroung...");
+            }}
+          />
+        </div>
         <div className="show__second">
           <h2>Complete Dental Care</h2>
           <h3>
