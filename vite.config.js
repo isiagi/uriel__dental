b6e43d6b42af8d6739e5683cf7c6468dfe3ae405
isiagi <isiagigeofrey0@gmail.com>
2024-01-08 10:10:45 +0300

    fix crush

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,5 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    chunkSizeWarningLimit: 100,
-    rollupOptions: {
-      onwarn(warning, warn) {
-        if (warning.code === "MODULE_LEVEL_DIRECTIVE") {
-          return;
-        }
-        warn(warning);
-      },
-    },
-  },
   plugins: [react()],
 });
