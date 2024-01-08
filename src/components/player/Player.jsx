69a9a22508ff273ffd5202a0336be40d020a9da7
isiagi <isiagigeofrey0@gmail.com>
2024-01-08 13:27:33 +0300

    fix crush

--- src/components/player/Player.jsx
+++ src/components/player/Player.jsx
@@ -5,15 +5,15 @@ function Player({ video }) {
   return (
     <div>
       <ReactPlayer
-        width={"100%"}
-        height={"100%"}
+        width="100%"
+        height="100%"
+        style={{ maxWidth: "100%" }}
         url={video}
         pip={true}
         controls={true}
         light={false}
         playing={true}
       />
-      <source src={video} type="video/mp4" />
     </div>
   );
 }
