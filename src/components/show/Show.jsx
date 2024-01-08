3faf9ecfa46179a01fc0d83c30add578ffdff1d8
isiagi <isiagigeofrey0@gmail.com>
2024-01-08 16:01:28 +0300

    add ui

--- src/components/show/Show.jsx
+++ src/components/show/Show.jsx
@@ -1,26 +1,14 @@
 import "./show.css";
 import Button from "../button/Button";
-import ReactPlayer from "react-player";
+import aui from "../../assets1/videos/uriel1.mp4";
+import Player from "../player/Player";
 
 function Show() {
   return (
     <div className="show__container">
       <div className="middle__class show__wrapper">
         <div className="show__image__div">
-          <ReactPlayer
-            width={"100%"}
-            height={"100%"}
-            url={
-              "https://res.cloudinary.com/isiagi/video/upload/v1704643602/loid5v7tpvdeci02hj5i.mp4"
-            }
-            pip={true}
-            controls={true}
-            light={false}
-            playing={true}
-            onError={() => {
-              console.log("Something went wroung...");
-            }}
-          />
+          <Player video={aui} />
         </div>
         <div className="show__second">
           <h2>Complete Dental Care</h2>
